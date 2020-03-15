7a8b70bcbeae5bb5e28d9f89893b4be90fdecd21
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2020-03-15 11:37:41 +0700

    feat: add og for [country]

---
+++ api/countries/[country]/og.ts
@@ -0,0 +1,56 @@
+import { NowResponse, NowRequest } from "@now/node";
+import { getHtml } from "../../../util/template";
+import { writeTempFile, pathToFileURL } from "../../../util/file";
+import { getScreenshot } from "../../../util/chromium";
+import {
+  getTotalConfirmed,
+  getTotalRecovered,
+  getTotalDeaths,
+  getLastUpdate,
+  getDailyCases
+} from "../../../util/api";
+
+const isDev = process.env.NOW_REGION === "dev1";
+
+export default async function handler(req: NowRequest, res: NowResponse) {
+  try {
+    const country = req.query.country as string;
+    const width = parseInt(req.query.width as string, 10) || 1200;
+    const height = parseInt(req.query.height as string, 10) || 627;
+    const isHtmlDebug =
+      isDev &&
+      (process.env.OG_HTML_DEBUG === "1" || req.query.debug === "true");
+    const [confirmed, recovered, deaths, lastUpdate] = await Promise.all([
+      getTotalConfirmed(country),
+      getTotalRecovered(country),
+      getTotalDeaths(country),
+      getLastUpdate(country)
+    ]);
+    const html = getHtml({
+      confirmed,
+      recovered,
+      deaths,
+      lastUpdate,
+      dailyCases: [],
+      width,
+      height,
+      countryRegion: getCountryName(country)
+    });
+    if (isHtmlDebug) {
+      res.setHeader("Content-Type", "text/html");
+      res.end(html);
+      return;
+    }
+    const text = country;
+    const filePath = await writeTempFile(text, html);
+    const fileUrl = pathToFileURL(filePath);
+    const file = await getScreenshot(fileUrl, isDev, width, height);
+    res.setHeader("Content-Type", `image/png`);
+    res.end(file);
+  } catch (e) {
+    res.statusCode = 500;
+    res.setHeader("Content-Type", "text/html");
+    res.end("<h1>Internal Error</h1><p>Sorry, there was a problem</p>");
+    console.error(e);
+  }
+}
--- util/template.ts
+++ util/template.ts
@@ -104,6 +104,7 @@ interface ParsedRequest {
   dailyCases: any[];
   width: number;
   height: number;
+  countryRegion?: string;
 }
 
 export function getHtml(parsedReq: ParsedRequest) {
@@ -114,7 +115,8 @@ export function getHtml(parsedReq: ParsedRequest) {
     lastUpdate,
     dailyCases,
     width,
-    height
+    height,
+    countryRegion
   } = parsedReq;
   return `<!DOCTYPE html>
 <html>
@@ -129,52 +131,62 @@ export function getHtml(parsedReq: ParsedRequest) {
   </head>
   <body>
   
-  <!-- width, height and stroke-width attributes must be defined on the target SVG -->
-<svg class="sparkline black" width="${width}" height="${height}" stroke-width="4" style="position: absolute; z-index:-1; opacity:0.5;"></svg>
-<svg class="sparkline green" width="${width}" height="${height}" stroke-width="4" style="position: absolute; z-index:-1; opacity:0.5; top: ${height -
-    Math.floor((recovered / confirmed) * height)}px;"></svg>
-<svg class="sparkline red" width="${width}" height="${height}" stroke-width="2" style="position: absolute; z-index:-1; opacity:0.5; top: ${height -
-    Math.floor((deaths / confirmed) * height)}px;" stroke-dasharray="5,5"></svg>
-<svg class="sparkline orange" width="${width}" height="${height}" stroke-width="4" style="position: absolute; z-index:-1; opacity:0.5; top: ${height -
-    Math.floor(
-      (dailyCases.slice(-1)[0].otherLocations / confirmed) * height
-    )}px;"></svg>
-
-<script>
-
-const svgs = document.querySelectorAll(".sparkline")
-sparkline.sparkline(svgs[0], [${dailyCases
-    .map(d => d.totalConfirmed)
-    .join(", ")}]);
-svgs[1].setAttribute("height", Math.floor(${
-    dailyCases.slice(-1)[0].totalRecovered
-  }/${confirmed} * ${height}))
-sparkline.sparkline(svgs[1], [${dailyCases
-    .map(d => d.totalRecovered || 0)
-    .join(", ")}]);
-svgs[2].setAttribute("height", Math.floor(${deaths}/${confirmed} * ${height}))
-sparkline.sparkline(svgs[2], [100,100,100]);
-svgs[3].setAttribute("height", Math.floor(${
-    dailyCases.slice(-1)[0].otherLocations
-  }/${confirmed} *  ${height}))
-sparkline.sparkline(svgs[3], [${dailyCases
-    .map(d => d.otherLocations || 0)
-    .join(", ")}]);
-</script>
+  ${
+    dailyCases.length > 0
+      ? `  <!-- width, height and stroke-width attributes must be defined on the target SVG -->
+    <svg class="sparkline black" width="${width}" height="${height}" stroke-width="4" style="position: absolute; z-index:-1; opacity:0.5;"></svg>
+    <svg class="sparkline green" width="${width}" height="${height}" stroke-width="4" style="position: absolute; z-index:-1; opacity:0.5; top: ${height -
+          Math.floor((recovered / confirmed) * height)}px;"></svg>
+    <svg class="sparkline red" width="${width}" height="${height}" stroke-width="2" style="position: absolute; z-index:-1; opacity:0.5; top: ${height -
+          Math.floor(
+            (deaths / confirmed) * height
+          )}px;" stroke-dasharray="5,5"></svg>
+    <svg class="sparkline orange" width="${width}" height="${height}" stroke-width="4" style="position: absolute; z-index:-1; opacity:0.5; top: ${height -
+          Math.floor(
+            (dailyCases.slice(-1)[0].otherLocations / confirmed) * height
+          )}px;"></svg>
+    
+    <script>
+    
+    const svgs = document.querySelectorAll(".sparkline")
+    sparkline.sparkline(svgs[0], [${dailyCases
+      .map(d => d.totalConfirmed)
+      .join(", ")}, ${confirmed}]);
+    svgs[1].setAttribute("height", Math.floor(${
+      dailyCases.slice(-1)[0].totalRecovered
+    }/${confirmed} * ${height}))
+    sparkline.sparkline(svgs[1], [${dailyCases
+      .map(d => d.totalRecovered || 0)
+      .join(", ")}, ${recovered}]);
+    svgs[2].setAttribute("height", Math.floor(${deaths}/${confirmed} * ${height}))
+    sparkline.sparkline(svgs[2], [100,100,100]);
+    svgs[3].setAttribute("height", Math.floor(${
+      dailyCases.slice(-1)[0].otherLocations
+    }/${confirmed} *  ${height}))
+    sparkline.sparkline(svgs[3], [${dailyCases
+      .map(d => d.otherLocations || 0)
+      .join(", ")}, ${confirmed - (recovered + deaths)}]);
+    </script>`
+      : ""
+  }
       <div class="wrapper">
         <div class="data-wrapper font-inter" style="font-weight: bold; font-size: 2rem; justify-content: flex-start; padding: 2rem 2rem; display: flex;flex-direction: column;align-items: flex-start;">
           <h1 style="margin: 0;font-size: 2rem;font-weight: normal; letter-spacing: 1px;">COVID-19 API</h1>
-          <h2 style="margin: 0;">Global</h2>
+          <h2 style="margin: 0;">${countryRegion}</h2>
         </div>
         <div class="data-wrapper" style="justify-content: space-between; flex: 1; padding: 0 2rem;">
           <div class="data">
             <div class="heading font-inter">Confirmed</div>
             <div class="value font-inter">${confirmed}</div>
-            <div class="heading font-inter" style="font-size: 1.25rem;background: orange;color: white;line-height: 1.5;padding: 0 0.5rem;"><b style="margin: 0;">${
-              dailyCases.slice(-1)[0].otherLocations
-            }</b> outside China <b style="margin: 0;">(${Math.trunc(
-    (dailyCases.slice(-1)[0].otherLocations / confirmed) * 100
-  )}%)</b></div>
+            ${
+              dailyCases.length > 0
+                ? `<div class="heading font-inter" style="font-size: 1.25rem;background: orange;color: white;line-height: 1.5;padding: 0 0.5rem;"><b style="margin: 0;">${
+                    dailyCases.slice(-1)[0].otherLocations
+                  }</b> outside China <b style="margin: 0;">(${Math.trunc(
+                    (dailyCases.slice(-1)[0].otherLocations / confirmed) * 100
+                  )}%)</b></div>`
+                : ""
+            }
           </div>
 
           <div class="data">
