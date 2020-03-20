d260ba19cb6f1f509a83fefdcea11e25a39a4045
Anth Lubic <anthlubic@gmail.com>
2020-03-20 07:38:52 -0400

    fix more countries and add tests

---
+++ util/countries.test.ts
@@ -0,0 +1,21 @@
+import { countries, iso3, getCountryName } from './countries';
+
+describe('getCountryName', () => {
+
+  test('returns undefined when countryName is invalid', () => {
+    expect(getCountryName('')).toBe(undefined)
+  })
+
+  test('returns countryName if it exists in countries', () => {
+    expect(getCountryName('United States')).toBe('United States');
+  })
+
+  test('converts ISO2 to jhu country name', () => {
+    expect(getCountryName('PL')).toBe('Poland');
+    expect(getCountryName('KR')).toBe('Korea, South');
+  })
+
+  test('converts ISO3 to jhu country name', () => {
+    expect(getCountryName('DEU')).toBe('Germany');
+  })
+})
