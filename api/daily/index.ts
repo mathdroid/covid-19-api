5d929534e48b06fbf7646b67f9c9032701fb7539
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2020-03-24 00:48:42 +0700

    add china/not separation

--- api/daily/index.ts
+++ api/daily/index.ts
@@ -7,12 +7,66 @@ export default async (_, response: NowResponse) => {
       (acc, cur) => ({
         ...acc,
         [cur.date]: {
-          confirmed:
-            cur.confirmed + ((acc[cur.date] && acc[cur.date].confirmed) || 0),
-          deltaConfirmed:
-            cur.deltaConfirmed +
-            ((acc[cur.date] && acc[cur.date].deltaConfirmed) || 0),
-          deaths: cur.deaths + ((acc[cur.date] && acc[cur.date].deaths) || 0),
+          confirmed: {
+            total:
+              cur.confirmed +
+              ((acc[cur.date] &&
+                acc[cur.date].confirmed &&
+                acc[cur.date].confirmed.total) ||
+                0),
+            china:
+              (cur.countryRegion === "China" ? cur.confirmed : 0) +
+              ((acc[cur.date] &&
+                acc[cur.date].confirmed &&
+                acc[cur.date].confirmed.china) ||
+                0),
+            outsideChina:
+              (cur.countryRegion !== "China" ? cur.confirmed : 0) +
+              ((acc[cur.date] &&
+                acc[cur.date].confirmed &&
+                acc[cur.date].confirmed.outsideChina) ||
+                0)
+          },
+          deltaConfirmed: {
+            total:
+              cur.deltaConfirmed +
+              ((acc[cur.date] &&
+                acc[cur.date].deltaConfirmed &&
+                acc[cur.date].deltaConfirmed.total) ||
+                0),
+            china:
+              (cur.countryRegion === "China" ? cur.deltaConfirmed : 0) +
+              ((acc[cur.date] &&
+                acc[cur.date].deltaConfirmed &&
+                acc[cur.date].deltaConfirmed.china) ||
+                0),
+            outsideChina:
+              (cur.countryRegion !== "China" ? cur.deltaConfirmed : 0) +
+              ((acc[cur.date] &&
+                acc[cur.date].deltaConfirmed &&
+                acc[cur.date].deltaConfirmed.outsideChina) ||
+                0)
+          },
+          deaths: {
+            total:
+              cur.deaths +
+              ((acc[cur.date] &&
+                acc[cur.date].deaths &&
+                acc[cur.date].deaths.total) ||
+                0),
+            china:
+              (cur.countryRegion === "China" ? cur.deaths : 0) +
+              ((acc[cur.date] &&
+                acc[cur.date].deaths &&
+                acc[cur.date].deaths.china) ||
+                0),
+            outsideChina:
+              (cur.countryRegion !== "China" ? cur.deaths : 0) +
+              ((acc[cur.date] &&
+                acc[cur.date].deaths &&
+                acc[cur.date].deaths.outsideChina) ||
+                0)
+          },
           active: cur.active + ((acc[cur.date] && acc[cur.date].active) || 0),
           deltaRecovered:
             cur.deltaRecovered +
