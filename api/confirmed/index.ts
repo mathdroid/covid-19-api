8b7fdf11769ff25246614e472a4f603486ffa605
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2020-03-23 18:13:23 +0700

    add byCountry query to confirmed

--- api/confirmed/index.ts
+++ api/confirmed/index.ts
@@ -1,4 +1,4 @@
-import { NowResponse } from "@now/node";
+import { NowResponse, NowRequest } from "@now/node";
 import {
   fetchFeatures,
   attributeSpreader,
@@ -9,12 +9,51 @@ import {
 import { endpoints } from "../../util/endpoints";
 import { queryConfirmed } from "../../util/query";
 
-export default async (_, response: NowResponse) => {
-  response.json(
-    (await fetchFeatures(endpoints.cases, queryConfirmed()))
-      .map(attributeSpreader)
-      .map(normalizeKeys)
-      .map(matchCountryCode)
-      .map(getIso3Code)
-  );
+const sumObject = (base, newValue) => {
+  for (const key in newValue) {
+    if (typeof base[key] === "number" && typeof newValue[key] === "number") {
+      base[key] += newValue[key];
+    }
+  }
+  return base;
+};
+
+const groupByCountryRegion = data => {
+  const countriesHash = data.reduce((acc, cur) => {
+    if (!acc[cur.countryRegion]) {
+      return {
+        ...acc,
+        [cur.countryRegion]: {
+          countryRegion: cur.countryRegion,
+          confirmed: cur.confirmed,
+          recovered: cur.recovered,
+          deaths: cur.deaths,
+          active: cur.active
+        }
+      };
+    } else {
+      return {
+        ...acc,
+        [cur.countryRegion]: sumObject(acc[cur.countryRegion], {
+          countryRegion: cur.countryRegion,
+          confirmed: cur.confirmed,
+          recovered: cur.recovered,
+          deaths: cur.deaths,
+          active: cur.active
+        })
+      };
+    }
+  }, {});
+  return Object.values(countriesHash);
+};
+
+export default async (request: NowRequest, response: NowResponse) => {
+  const shouldGroupByCountryRegion = request.query["byCountry"] === "true";
+
+  const data = (await fetchFeatures(endpoints.cases, queryConfirmed()))
+    .map(attributeSpreader)
+    .map(normalizeKeys)
+    .map(matchCountryCode)
+    .map(getIso3Code);
+  response.json(shouldGroupByCountryRegion ? groupByCountryRegion(data) : data);
 };
