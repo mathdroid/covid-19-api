d4eb14c53a275e3dc57843af5f0031a9f26c14f6
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2020-03-24 01:11:35 +0700

    backward compatibility

--- api/daily/index.ts
+++ api/daily/index.ts
@@ -3,37 +3,55 @@ import { getConfirmedGraph } from "../../util/api";
 
 export default async (_, response: NowResponse) => {
   const result = Object.entries(
-    (await getConfirmedGraph()).reduce(
-      (acc, cur) => ({
+    (await getConfirmedGraph()).reduce((acc, cur) => {
+      const totalConfirmed =
+        cur.confirmed +
+        ((acc[cur.date] &&
+          acc[cur.date].confirmed &&
+          acc[cur.date].confirmed.total) ||
+          0);
+      const mainlandChina =
+        (cur.countryRegion === "China" ? cur.confirmed : 0) +
+        ((acc[cur.date] &&
+          acc[cur.date].confirmed &&
+          acc[cur.date].confirmed.china) ||
+          0);
+
+      const otherLocations =
+        (cur.countryRegion !== "China" ? cur.confirmed : 0) +
+        ((acc[cur.date] &&
+          acc[cur.date].confirmed &&
+          acc[cur.date].confirmed.outsideChina) ||
+          0);
+      const deltaConfirmed =
+        cur.deltaConfirmed +
+        ((acc[cur.date] &&
+          acc[cur.date].deltaConfirmed &&
+          acc[cur.date].deltaConfirmed.total) ||
+          0);
+
+      const totalRecovered =
+        cur.recovered +
+        ((acc[cur.date] &&
+          acc[cur.date].recovered &&
+          acc[cur.date].recovered.total) ||
+          0);
+
+      return {
         ...acc,
         [cur.date]: {
+          totalConfirmed,
+          mainlandChina,
+          otherLocations,
+          deltaConfirmed,
+          totalRecovered,
           confirmed: {
-            total:
-              cur.confirmed +
-              ((acc[cur.date] &&
-                acc[cur.date].confirmed &&
-                acc[cur.date].confirmed.total) ||
-                0),
-            china:
-              (cur.countryRegion === "China" ? cur.confirmed : 0) +
-              ((acc[cur.date] &&
-                acc[cur.date].confirmed &&
-                acc[cur.date].confirmed.china) ||
-                0),
-            outsideChina:
-              (cur.countryRegion !== "China" ? cur.confirmed : 0) +
-              ((acc[cur.date] &&
-                acc[cur.date].confirmed &&
-                acc[cur.date].confirmed.outsideChina) ||
-                0)
+            total: totalConfirmed,
+            china: mainlandChina,
+            outsideChina: otherLocations
           },
-          deltaConfirmed: {
-            total:
-              cur.deltaConfirmed +
-              ((acc[cur.date] &&
-                acc[cur.date].deltaConfirmed &&
-                acc[cur.date].deltaConfirmed.total) ||
-                0),
+          deltaConfirmedDetail: {
+            total: deltaConfirmed,
             china:
               (cur.countryRegion === "China" ? cur.deltaConfirmed : 0) +
               ((acc[cur.date] &&
@@ -67,6 +85,21 @@ export default async (_, response: NowResponse) => {
                 acc[cur.date].deaths.outsideChina) ||
                 0)
           },
+          recovered: {
+            total: totalRecovered,
+            china:
+              (cur.countryRegion === "China" ? cur.recovered : 0) +
+              ((acc[cur.date] &&
+                acc[cur.date].recovered &&
+                acc[cur.date].recovered.china) ||
+                0),
+            outsideChina:
+              (cur.countryRegion !== "China" ? cur.recovered : 0) +
+              ((acc[cur.date] &&
+                acc[cur.date].recovered &&
+                acc[cur.date].recovered.outsideChina) ||
+                0)
+          },
           active: cur.active + ((acc[cur.date] && acc[cur.date].active) || 0),
           deltaRecovered:
             cur.deltaRecovered +
@@ -78,14 +111,13 @@ export default async (_, response: NowResponse) => {
             cur.peopleTested +
             ((acc[cur.date] && acc[cur.date].peopleTested) || 0)
         }
-      }),
-      {}
-    )
+      };
+    }, {})
   );
   response.json(
-    result.map(([date, data]: [string, any]) => ({
+    result.map(([reportDate, data]: [string, any]) => ({
       ...data,
-      date
+      reportDate
     }))
   );
 };
