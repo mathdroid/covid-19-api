e49a144ba208e5c68ce10574f18a23a5932a8598
spiritbro1 <rinoakbr@gmail.com>
2020-10-18 16:19:18 +0700

    add isError

--- api/statusbadge.ts
+++ api/statusbadge.ts
@@ -3,14 +3,25 @@ import unfetch from "isomorphic-unfetch";
 import withRetry from "@zeit/fetch-retry";
 const fetch = withRetry(unfetch);
 export default async function handler(req: NowRequest, res: NowResponse) {
-  const result = await fetch(
-    "https://github.com/mathdroid/covid-19-api/releases/download/saved_data/status.json"
-  );
-  const badge = await result.json();
-  res.json({
-    schemaVersion: 1,
-    label: (<string>req.query.status).replace(/_/g, " "),
-    message: badge[req.query.status],
-    color: req.query.status === "error" ? "red" : "green",
-  });
+  try{
+    const result = await fetch(
+      "https://github.com/spiritbro1/covid-19-api/releases/download/saved_data/status.json"
+    );
+    const badge = await result.json();
+    res.json({
+      schemaVersion: 1,
+      label: (<string>req.query.status).replace(/_/g, " "),
+      message: badge[req.query.status],
+      color: req.query.status === "error" ? "red" : "green",
+    });
+  }catch(e){
+    res.json({
+      isError:true,
+      schemaVersion: 1,
+      label: (<string>req.query.status).replace(/_/g, " "),
+      message: "error",
+      color:  "red" ,
+    });
+  }
+  
 }
