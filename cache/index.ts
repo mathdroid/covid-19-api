f67d07b3d19c34427798eac53f7b4bef60fcffe6
spiritbro1 <62529025+spiritbro1@users.noreply.github.com>
2020-10-18 09:04:52 +0700

    one repo to rule them all fixed (#1)
    * change main.yml
    
    * change main.yml using node version 14
    
    * check iter
    
    * bismillah terakhir bisa
    
    * now we can get data from release

--- cache/index.ts
+++ cache/index.ts
@@ -1,23 +1,17 @@
-import {daily} from './daily'
-import {og} from './og'
+import { daily } from "./daily";
+import { og } from "./og";
 import * as fs from "fs";
 import { promisify } from "util";
-import format  from 'date-fns/format';
-(async ()=>{
-    const [
-        statusOg,
-        statusDaily
-      ] = await Promise.all([
-        og(),
-        daily()
-      ]);
-      const write = promisify(fs.writeFile);
-      const writeReadme=`{
+import format from "date-fns/format";
+(async () => {
+  const [statusOg, statusDaily] = await Promise.all([og(), daily()]);
+
+  const write = promisify(fs.writeFile);
+  const writeReadme = `{
 "date":"${format(new Date(), "cccc, dd LLLL yyyy HH:mm:ss")}",
-"og_status":"${statusOg?"success":"error"}",
-"daily_status":"${statusDaily?"success":"error"}"
+"og_status":"${statusOg ? "success" : "error"}",
+"daily_status":"${statusDaily ? "success" : "error"}"
 }  
       `;
-      await write("cache/status.json", writeReadme);
-    
-})()
+  await write("cache/status.json", writeReadme);
+})();
